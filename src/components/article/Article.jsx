afb35b3f01194ab02786d270a5290f89e86fe391
Christian Ruiz <christianjavieruizcortes@gmail.com>
2023-01-26 16:55:33 -0500

    small details

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,18 +1,20 @@
 import "./Article.css";
 
 const Article = ({ imgUrl, date, text }) => (
-  <div className="gpt3__blog-container_article">
-    <div className="gpt3__blog-container_article-image">
-      <img src={imgUrl} alt="blog_image" />
-    </div>
-    <div className="gpt3__blog-container_article-content">
-      <div>
-        <p>{date}</p>
-        <h3>{text}</h3>
+  <a href="#">
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog_image" />
       </div>
-      <p>Read Full Article</p>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
     </div>
-  </div>
+  </a>
 );
 
 export default Article;
